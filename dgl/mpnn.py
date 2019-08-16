9ca89cd3951aa827bc9db9be9ff35b96393d5d54
Yufei Luo <421814225@qq.com>
2019-08-16 14:49:18 +0800

    Add files via upload

---
+++ dgl/mpnn.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+from torch.utils.data import dataloader
+import dgl
+import dgl.function as fn
+import dgl.nn.pytorch as dgl_nn
+
+
+import os
+from os import listdir
+from os.path import isfile, join
+import networkx as nx
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import ChemicalFeatures, AllChem
+from rdkit import RDConfig
+import numpy as np
+import pickle
+
+
+class NNConvLayer(nn.Module):
+    """MPNN Conv Layer
+
+    Paramters
+    ---------
+
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 edge_net,
+                 aggr='sum',
+                 root_weight=True,
+                 bias=True):
+        super(NNConvLayer, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.edge_net = edge_net
+        self.aggr = aggr
+
+        if root_weight:
+            self.root = Parameter(torch.Tensor(in_channels, out_channels))
+        else:
+            self.register_parameter('root', None)
+
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        if self.root is not None:
+            nn.init.xavier_normal_(self.root.data, gain=1.414)
+        if self.bias is not None:
+            self.bias.data.zero_()
+        for m in self.edge_net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight.data, gain=1.414)
+
+    def message(self, edges):
+        return {'m': torch.matmul(edges.src['h'].unsqueeze(1), edges.data['w']).squeeze(1)}
+
+    def reduce(self, nodes):
+        #return {'aggr_out': eval("torch.{self.aggr}(nodes.mailbox['m'], 1)")}
+        if self.aggr == 'sum':
+            return {'aggr_out': torch.sum(nodes.mailbox['m'], 1)}
+        if self.aggr =='mean':
+            return {'aggr_out': torch.mean(nodes.mailbox['m'], 1)}
+            
+    def apply_node_func(self, nodes):
+        aggr_out = nodes.data['aggr_out']
+        if self.root is not None:
+            aggr_out = torch.mm(nodes.data['h'], self.root) + aggr_out
+
+        if self.bias is not None:
+            aggr_out = aggr_out + self.bias
+
+        return {'h': aggr_out}
+
+    def forward(self, g, h, e):
+        h = h.unsqueeze(-1) if h.dim() == 1 else h
+        e = e.unsqueeze(-1) if e.dim() == 1 else e
+
+        g.ndata['h'] = h
+        g.edata['w'] = self.edge_net(e).view(-1, self.in_channels, self.out_channels)
+        g.update_all(self.message, self.reduce, self.apply_node_func)
+        return g.ndata.pop('h')
+
+
+class MPNNModel(nn.Module):
+    """MPNN model"""
+
+    def __init__(self,
+                 node_input_dim=15,
+                 edge_input_dim=5,
+                 output_dim=12,
+                 node_hidden_dim=64,
+                 edge_hidden_dim=128,
+                 num_step_message_passing=6,
+                 num_step_set2set=6,
+                 num_layer_set2set=3,
+                 device=torch.device("cuda")):
+        """model parameters setting
+
+        Paramters
+        ---------
+
+        """
+        super(MPNNModel, self).__init__()
+        self.num_step_message_passing = num_step_message_passing
+        self.lin0 = nn.Linear(node_input_dim, node_hidden_dim)
+        self.device = device
+        edge_network = nn.Sequential(
+            nn.Linear(edge_input_dim, edge_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(edge_hidden_dim, node_hidden_dim * node_hidden_dim)
+        )
+        self.conv = NNConvLayer(in_channels=node_hidden_dim, out_channels=node_hidden_dim, edge_net=edge_network,
+                                aggr='mean', root_weight=False)
+        self.gru = nn.GRU(node_hidden_dim, node_hidden_dim)
+
+        self.set2set = dgl_nn.glob.Set2Set(node_hidden_dim, num_step_set2set, num_layer_set2set)
+        self.lin1 = nn.Linear(2 * node_hidden_dim, node_hidden_dim)
+        self.lin2 = nn.Linear(node_hidden_dim, output_dim)
+
+    def forward(self, g):
+        h = g.ndata['n_feat']
+        #print(h)
+        h = h.to(self.device)
+        out = F.relu(self.lin0(h))
+        h = out.unsqueeze(0)
+
+        for i in range(self.num_step_message_passing):
+            m = F.relu(self.conv(g, out, g.edata['e_feat']))
+            out, h = self.gru(m.unsqueeze(0), h)
+            out = out.squeeze(0)
+
+        out = self.set2set(out, g)
+        out = F.relu(self.lin1(out))
+        out = self.lin2(out)
+        return out
+        
+if __name__ == "__main__":
+    g = dgl.DGLGraph()
+    g.add_nodes(2)
+    g.add_edges([0, 0, 1, 1], [1, 0, 1, 0])
+    g.edata["e_feat"] = th.tensor([1.0, 3.0, 2.0, 4.0]).reshape(-1, 1)
+    g.ndata["n_feat"] = th.LongTensor([1, 2])
+    model = MGCNModel(dim=2, edge_dim=2)
+    node = model(g)
+    print(node)
