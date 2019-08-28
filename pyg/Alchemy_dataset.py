659c59fbbe93d406f8b3e0711e5a048e58c9c43c
geekinglcq <lcqgeek@live.com>
2019-08-28 15:10:46 +0800

    :bug: rename data file

--- pyg/Alchemy_dataset.py
+++ pyg/Alchemy_dataset.py
@@ -17,38 +17,52 @@ import pathlib
 import pandas as pd
 
 _urls = {
-        'dev': 'https://alchemy.tencent.com/data/dev.zip',
-        'valid': 'https://alchemy.tencent.com/data/valid.zip',
-        'test': 'https://alchemy.tencent.com/data/test.zip',
-        }
+    'dev': 'https://alchemy.tencent.com/data/dev.zip',
+    'valid': 'https://alchemy.tencent.com/data/valid.zip',
+    'test': 'https://alchemy.tencent.com/data/test.zip',
+}
+
 
 class TencentAlchemyDataset(InMemoryDataset):
     fdef_name = osp.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')
     chem_feature_factory = ChemicalFeatures.BuildFeatureFactory(fdef_name)
 
-    def __init__(self, root, mode='dev', transform=None, pre_transform=None, pre_filter=None):
+    def __init__(self,
+                 root,
+                 mode='dev',
+                 transform=None,
+                 pre_transform=None,
+                 pre_filter=None):
         self.mode = mode
         assert mode in _urls
-        super(TencentAlchemyDataset, self).__init__(root, transform, pre_transform, pre_filter)
+        super(TencentAlchemyDataset, self).__init__(root, transform,
+                                                    pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self):
-        return [self.mode + '/sdf', self.mode + '/train.csv'] if self.mode == 'dev' else [self.mode + '/sdf', ]
+        return [self.mode + '/sdf', self.mode +
+                '/dev_target.csv'] if self.mode == 'dev' else [
+                    self.mode + '/sdf',
+                ]
 
     @property
     def processed_file_names(self):
         return 'TencentAlchemy_%s.pt' % self.mode
 
     def download(self):
-        raise NotImplementedError('please download and unzip dataset from %s, and put it at %s' % (_urls[self.mode], self.raw_dir))
+        pass
+        # raise NotImplementedError('please download and unzip dataset from %s, and put it at %s' % (_urls[self.mode], self.raw_dir))
 
     def alchemy_nodes(self, g):
         feat = []
         for n, d in g.nodes(data=True):
             h_t = []
             # Atom type (One-hot H, C, N, O F)
-            h_t += [int(d['a_type'] == x) for x in ['H', 'C', 'N', 'O', 'F', 'S', 'Cl']]
+            h_t += [
+                int(d['a_type'] == x)
+                for x in ['H', 'C', 'N', 'O', 'F', 'S', 'Cl']
+            ]
             # Atomic number
             h_t.append(d['a_num'])
             # Acceptor
@@ -69,7 +83,7 @@ class TencentAlchemyDataset(InMemoryDataset):
         return node_attr
 
     def alchemy_edges(self, g):
-        e={}
+        e = {}
         for n1, n2, d in g.edges(data=True):
             e_t = [int(d['b_type'] == x)
                     for x in (Chem.rdchem.BondType.SINGLE, \
@@ -105,9 +119,14 @@ class TencentAlchemyDataset(InMemoryDataset):
         geom = mol.GetConformers()[0].GetPositions()
         for i in range(mol.GetNumAtoms()):
             atom_i = mol.GetAtomWithIdx(i)
-            g.add_node(i, a_type=atom_i.GetSymbol(), a_num=atom_i.GetAtomicNum(), acceptor=0, donor=0,
-                    aromatic=atom_i.GetIsAromatic(), hybridization=atom_i.GetHybridization(),
-                    num_h=atom_i.GetTotalNumHs())
+            g.add_node(i,
+                       a_type=atom_i.GetSymbol(),
+                       a_num=atom_i.GetAtomicNum(),
+                       acceptor=0,
+                       donor=0,
+                       aromatic=atom_i.GetIsAromatic(),
+                       hybridization=atom_i.GetHybridization(),
+                       num_h=atom_i.GetTotalNumHs())
 
         for i in range(len(feats)):
             if feats[i].GetFamily() == 'Donor':
@@ -128,18 +147,22 @@ class TencentAlchemyDataset(InMemoryDataset):
         node_attr = self.alchemy_nodes(g)
         edge_index, edge_attr = self.alchemy_edges(g)
         data = Data(
-                x=node_attr,
-                pos=torch.FloatTensor(geom),
-                edge_index=edge_index,
-                edge_attr=edge_attr,
-                y=l,
-                )
+            x=node_attr,
+            pos=torch.FloatTensor(geom),
+            edge_index=edge_index,
+            edge_attr=edge_attr,
+            y=l,
+        )
         return data
 
     def process(self):
         if self.mode == 'dev':
-            self.target = pd.read_csv(self.raw_paths[1], index_col=0,
-                    usecols=['gdb_idx',] + ['property_%d' % x for x in range(12)])
+            self.target = pd.read_csv(self.raw_paths[1],
+                                      index_col=0,
+                                      usecols=[
+                                          'gdb_idx',
+                                      ] +
+                                      ['property_%d' % x for x in range(12)])
             self.target = self.target[['property_%d' % x for x in range(12)]]
         sdf_dir = pathlib.Path(self.raw_paths[0])
         data_list = []
@@ -156,5 +179,3 @@ class TencentAlchemyDataset(InMemoryDataset):
 
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
-
-
--- pyg/mpnn.py
+++ pyg/mpnn.py
@@ -10,10 +10,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import torch_geometric.transforms as T
-from Alchemy_dataset import TencentAlchemyDataset
+from tdataset import TencentAlchemyDataset
 from torch_geometric.nn import NNConv, Set2Set
 from torch_geometric.data import DataLoader
 from torch_geometric.utils import remove_self_loops
+
+from datetime import datetime
+import time
+import logging
 
 import pandas as pd
 
@@ -44,19 +48,11 @@ class Complete(object):
         return data
 
 
-transform = T.Compose([Complete(), T.Distance(norm=False)])
-train_dataset = TencentAlchemyDataset(root='data-bin', mode='dev', transform=transform).shuffle()
-valid_dataset = TencentAlchemyDataset(root='data-bin', mode='valid', transform=transform)
-
-
-valid_loader = DataLoader(valid_dataset, batch_size=64)
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-
 class MPNN(torch.nn.Module):
     def __init__(self,
                  node_input_dim=15,
                  edge_input_dim=5,
-                 output_dim=12,
+                 output_dim=1,
                  node_hidden_dim=64,
                  edge_hidden_dim=128,
                  num_step_message_passing=6,
@@ -65,14 +61,17 @@ class MPNN(torch.nn.Module):
         self.num_step_message_passing = num_step_message_passing
         self.lin0 = nn.Linear(node_input_dim, node_hidden_dim)
         edge_network = nn.Sequential(
-                nn.Linear(edge_input_dim, edge_hidden_dim),
-                nn.ReLU(),
-                nn.Linear(edge_hidden_dim, node_hidden_dim * node_hidden_dim)
-                )
-        self.conv = NNConv(node_hidden_dim, node_hidden_dim, edge_network, aggr='mean', root_weight=False)
+            nn.Linear(edge_input_dim, edge_hidden_dim), nn.ReLU(),
+            nn.Linear(edge_hidden_dim, node_hidden_dim * node_hidden_dim))
+        self.conv = NNConv(node_hidden_dim,
+                           node_hidden_dim,
+                           edge_network,
+                           aggr='mean',
+                           root_weight=False)
         self.gru = nn.GRU(node_hidden_dim, node_hidden_dim)
 
-        self.set2set = Set2Set(node_hidden_dim, processing_steps=num_step_set2set)
+        self.set2set = Set2Set(node_hidden_dim,
+                               processing_steps=num_step_set2set)
         self.lin1 = nn.Linear(2 * node_hidden_dim, node_hidden_dim)
         self.lin2 = nn.Linear(node_hidden_dim, output_dim)
 
@@ -90,46 +89,96 @@ class MPNN(torch.nn.Module):
         out = self.lin2(out)
         return out
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = MPNN(node_input_dim=train_dataset.num_features).to(device)
-print(model)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+def run(prop="homo", gpuid="0", epoch=500, dataset="t2", size=100000):
 
+    # set logger
+    task_name = "MPNN_%s_%s_%s" % (dataset, prop,
+                                   datetime.now().strftime("%m%d_%H%M%S"))
+    logname = "./logs/%s.log" % (task_name)
+    log = logging.getLogger(task_name)
+    log.setLevel(logging.INFO)
+    fmt = "%(asctime)-s %(levelname)s %(filename)s %(message)s"
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    handler = logging.FileHandler(filename=logname)  # output to file
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter(fmt, datefmt))
+    log.addHandler(handler)
+    chler = logging.StreamHandler()  # print to console
+    chler.setFormatter(logging.Formatter(fmt, datefmt))
+    chler.setLevel(logging.INFO)
+    log.addHandler(chler)
+    log.info("Experiment of model: %s, dataset size: %d" % (task_name, size))
 
-def train(epoch):
+    device = torch.device("cuda:%s" % (gpuid))
+    transform = T.Compose([Complete(), T.Distance(norm=False)])
+    dataset = TencentAlchemyDataset(root='./tdata/',
+                                    mode='dev',
+                                    dataset=dataset,
+                                    prop=prop,
+                                    transform=transform).shuffle()
+    dataset = dataset[:size]
+    trainset = dataset[:size - 20000]
+    valset = dataset[size - 20000:size - 10000]
+    testset = dataset[size - 10000:]
+    train_loader = DataLoader(trainset, batch_size=64)
+    val_loader = DataLoader(valset, batch_size=64)
+    test_loader = DataLoader(testset, batch_size=64)
+    model = MPNN(node_input_dim=trainset.num_features).to(device)
+    print(model)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0002)
     model.train()
     loss_all = 0
+    loss = 0
+    mae = 0
 
-    for data in train_loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        y_model = model(data)
-        loss = F.mse_loss(y_model, data.y)
-        loss.backward()
-        loss_all += loss.item() * data.num_graphs
-        optimizer.step()
-    return loss_all / len(train_loader.dataset)
+    st = time.time()
+    best_valid = float("inf")
+    for it in range(epoch):
+
+        # train
+        for data in train_loader:
+            data = data.to(device)
+            optimizer.zero_grad()
+            y_model = model(data)
+            loss = F.mse_loss(y_model, data.y)
+            mae += F.l1_loss(y_model, data.y).item()
+            loss.backward()
+            loss_all += loss.item() * data.num_graphs
+            optimizer.step()
+        loss = loss_all / len(train_loader)
+        train_score = mae / len(train_loader)
+        mae = 0
+        for data in val_loader:
+            data = data.to(device)
+            y_model = model(data)
+            mae += F.l1_loss(y_model, data.y).item()
+        valid_score = mae / len(val_loader)
+        mae = 0
+        for data in test_loader:
+            data = data.to(device)
+            y_model = model(data)
+            mae += F.l1_loss(y_model, data.y).item()
+        test_score = mae / len(test_loader)
+
+        log.info("Epoch {:2d}, train loss {:.7f}, test loss no, \
+                 train mae {:.7f}, val mae {:.7f}, test mae {:.7f}".format(
+            it, loss, train_score, valid_score, test_score))
+
+        if valid_score < best_valid:
+            best_valid = valid_score
+            related_test = test_score
+            ed = time.time()
+
+    log.info(
+        "Best val mae: {:.7f}  Related test mae: {:.7f}  Time cost: {:.0f}".
+        format(best_valid, related_test, ed - st))
 
 
-def test(loader):
-    model.eval()
-
-    targets = dict()
-    for data in loader:
-        data = data.to(device)
-        y_pred = model(data)
-        for i in range(len(data.y)):
-            targets[data.y[i].item()] = y_pred[i].tolist()
-    return targets
-
-epoch = 1
-print("training...")
-for epoch in range(epoch):
-    loss = train(epoch)
-    print('Epoch: {:03d}, Loss: {:.7f}'.format(epoch, loss))
-
-targets = test(valid_loader)
-df_targets = pd.DataFrame.from_dict(targets, orient="index", columns=['property_%d' % x for x in range(12)])
-df_targets.sort_index(inplace=True)
-df_targets.to_csv('targets.csv', index_label='gdb_idx')
+if __name__ == "__main__":
+    import sys
+    gpuid = sys.argv[1]
+    size = int(sys.argv[2])
+    dataset = sys.argv[3]
+    prop = sys.argv[4]
+    run(prop, gpuid, 500, dataset, size)
